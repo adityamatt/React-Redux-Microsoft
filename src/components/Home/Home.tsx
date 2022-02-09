fcbf9c3d5b70054a77e1011a3c0ac784b0c2f72f
Aditya Tiwari <aditya.tiwarics@gmail.com>
2022-02-09 09:25:26 +0530

    dynamic css

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,6 +9,34 @@ export const Home = (props: IHome) => {
       <p>
         To get started, edit <code>src/components/Home/Home.tsx</code> and save to reload.
       </p>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-light">
+        Light
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button>
+
+      <button type="button" className="btn btn-link">
+        Link
+      </button>
     </Fragment>
   )
 }
